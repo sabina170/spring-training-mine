a09e2c9bbbb717b1963f85ef3afafa60ba317af1
Sabina Omarova <sabomar170@gmail.com>
2022-09-11 16:17:17 -0400

    Main method

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,12 +1,32 @@
 package com.cydeo.bean_annotation;
 
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.config.ComputerConfig;
+import com.cydeo.bean_annotation.config.RandomConfig;
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext();
+        //Creating container by using Application Context
+        ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, RandomConfig.class);
+
+        //Creating container by using BeanFactory
+        BeanFactory context = new AnnotationConfigApplicationContext();
+
+//        Monitor theMonitor = container.getBean(Monitor.class);
+        SonyMonitor theMonitor = container.getBean(SonyMonitor.class);
+        Case theCase = container.getBean(Case.class);
+        Motherboard theMotherboard = container.getBean(Motherboard.class);
+
+        PC myPc = new PC(theCase,theMonitor,theMotherboard);
+
+        myPc.powerUp();
 
     }
 }
