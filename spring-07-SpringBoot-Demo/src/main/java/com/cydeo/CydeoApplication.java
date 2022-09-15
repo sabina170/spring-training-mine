9f37f09be57ce29b0c9d8aae7f33c58c1d85358d
Sabina Omarova <sabomar170@gmail.com>
2022-09-15 15:04:58 -0400

    Spring Boot demo is created

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CydeoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
+    }
+
+}
