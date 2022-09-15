001aa3984f4c3c9e82c6c648ec8557af1d7003a8
Sabina Omarova <sabomar170@gmail.com>
2022-09-15 16:44:57 -0400

    spring boot practice

--- spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -1,5 +1,7 @@
 package com.cydeo;
 
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -7,9 +9,21 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class CydeoApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
-    }
+		Comment comment  = new Comment();
+		comment.setAuthor("Johnson");
+		comment.setText("Spring Framework");
+
+		ApplicationContext container = SpringApplication.run(CydeoApplication.class, args);
+
+		CommentService commentService = container.getBean(CommentService.class);
+		commentService.publishComment(comment);
+
+		commentService.printConfigData();
+		commentService.printDbConfigData();
+	}
+
+
 
 }
