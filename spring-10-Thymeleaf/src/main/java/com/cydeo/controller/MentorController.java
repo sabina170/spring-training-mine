c477497d4b7c75b16a3accd9ea8f629397fb9868
Sabina Omarova <sabomar170@gmail.com>
2022-10-03 01:31:09 -0400

    Thymeleaf - Mentor is done

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,7 +4,12 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -13,9 +18,35 @@ public class MentorController {
     @GetMapping("/register") // localhost:9090/mentor/register
     public String register(Model model){
 
+        List<String> batches = Arrays.asList("JD1","JD2","JD3");
+
+        model.addAttribute("batches", batches);
+
         model.addAttribute("mentor", new Mentor());
 
         return "mentor/mentor-register";
     }
+
+//    @PostMapping("/confirm") - previous mentor object will come
+//    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+//        return "/mentor/mentor-confirmation";
+//    }
+
+//    @PostMapping("/confirm") - new empty mentor object will come, but it will happen in confirm end pint
+//    public String submitForm(@ModelAttribute("mentor") Mentor mentor, Model model) {
+//
+//        model.addAttribute("mentor", new Mentor());
+//        return "mentor/mentor-register";
+//    }
+
+    @PostMapping("/confirm")  //new empty mentor object will come, in the mentor/register end point
+   public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+        return "redirect:/mentor/register";
+   }
+
+
+
+
+
 
 }
