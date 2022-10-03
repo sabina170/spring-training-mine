d7fdc64d0cb6164d3bc71bac0814a748a83ebb64
Sabina Omarova <sabomar170@gmail.com>
2022-10-02 22:58:17 -0400

    Thymeleaf - Mentor

---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @RequestMapping("/register") // localhost:808/mentor/register
+    public String register(){
+        return "student/register";
+    }
+
+    @RequestMapping("/drop") // localhost:808/mentor/drop
+    public String drop(){
+        return "student/register";
+    }
+}
---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/model/Mentor.java
@@ -0,0 +1,23 @@
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mentor {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String batch;
+    private boolean graduated;
+    private String company;
+
+
+
+
+}
