139c3a89542027f39004b828dbe2a55aec2f64f6
Sabina Omarova <sabomar170@gmail.com>
2022-10-23 14:44:32 -0400

    Spring Data - creation table through the class

---
+++ spring-11-Data-TableCrestion/src/main/java/com/cydeo/entity/DebitAccount.java
@@ -0,0 +1,13 @@
+package com.cydeo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+
+public class DebitAccount extends Account {
+
+    private BigDecimal overDraftFee;
+
+}
