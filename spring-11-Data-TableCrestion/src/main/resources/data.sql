546347cc06257a1f4666a8b0f0d088ee5ca56aa4
Sabina Omarova <sabomar170@gmail.com>
2022-10-23 14:19:37 -0400

    Spring Data - creation table through the class

--- spring-11-Data-TableCrestion/src/main/resources/data.sql
+++ spring-11-Data-TableCrestion/src/main/resources/data.sql
@@ -1,2 +1,9 @@
 INSERT INTO EMPLOYEES(id, name)
 values(1, 'Mike');
+
+-- postgres is gonna insert the data in student table that is coming from entity package:
+INSERT INTO students(first_name,last_name,email)
+VALUES('Mike','Smith','mike@cydeo.com');
+
+INSERT INTO students(first_name,last_name,email)
+VALUES('Tom','Hanks','tom@cydeo.com');
