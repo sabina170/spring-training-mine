99762454bd7ddad6c2fc3e31f1c78b1c237b46b9
Sabina Omarova <sabomar170@gmail.com>
2022-10-24 15:36:56 -0400

    Spring Data Relationship

--- spring-12-Data-Relationship/src/main/java/com/cydeo/enity/Item.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/enity/Item.java
@@ -18,4 +18,8 @@ public class Item {
     private String name;
     private String code;
 
+    public Item(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
