5bdc96b2a63f66bd632a55c1cfcd88f97d6ec90c
Sabina Omarova <sabomar170@gmail.com>
2022-10-27 15:20:31 -0400

    Spring  - Data Queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,29 +2,30 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-    //Display all employees with email address "" :
+    //Display all employees with email address ""
     List<Employee> findByEmail(String email);
 
-    //Display all employees with firstName "" and lastName "",
-    // also show all employees with an email address:
-    List<Employee> findByFirAndLastNameOrEmail(String firstName, String lastName, String email);
+    //Display all employees with firstname "" and last name "",
+    //also show all employees with an email address ""
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstname,String lastname,String email);
 
-    //Display all employees with firstName is NOT "" :
-    List<Employee> findByFirstNameIsNot(String firstName);
+    //Display all employees that first name is not ""
+    List<Employee> findByFirstNameIsNot(String firstname);
 
-    //Display all employees where last name starts with "" :
+    //Display all employees where last name starts with ""
     List<Employee> findByLastNameStartsWith(String pattern);
 
-    //Display all employees with salaries higher than "" :
+    //Display all employees with salaries higher than ""
     List<Employee> findBySalaryGreaterThan(Integer salary);
 
-    //Display all employees with salaries less than "" :
+    //Display all employees with salaries less than ""
     List<Employee> findBySalaryLessThan(Integer salary);
 
     //Display all employees that has been hired between "" and ""
@@ -38,6 +39,17 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     //Display all employees that do not have email address
     List<Employee> findByEmailIsNull();
+
+
+    @Query("SELECT employee FROM Employee employee WHERE employee.email='amcnee1@google.es'")
+    Employee retrieveEmployeeDetail();
+
+
+    @Query("SELECT e.salary FROM Employee e WHERE e.email='amcnee1@google.es'")
+    Integer retrieveEmployeeSalary();
+
+
+
 
 
 
