0995de82aca6a5b9c0f03aa9ab93f1e064a081dd
Sabina Omarova <sabomar170@gmail.com>
2022-10-27 14:19:33 -0400

    Spring  - Data Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -0,0 +1,36 @@
+package com.cydeo;
+
+import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.RegionRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryDemo implements CommandLineRunner {
+
+    private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
+
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+        this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("-----------REGIONS-----------");
+
+        System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
+        System.out.println("findByCountryContaining:" + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainsOrderByRegion:" + regionRepository.findByCountryContainsOrderByRegion("United"));
+        System.out.println("findTop2ByCountry:" + regionRepository.findTop2ByCountry("United States"));
+        System.out.println("findTopByCountryContainsOrderByRegion:" + regionRepository.findTopByCountryContainsOrderByRegion("United States"));
+
+        System.out.println("-----------DEPARTMENTS----------");
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Furniture"));
+        System.out.println("findByDivision:" + departmentRepository.findByDivision("Health"));
+        System.out.println("findByDivisionEndsWith:" + departmentRepository.findByDivisionEndsWith("ics"));
+        System.out.println("findDistinctTop3ByDivisionContains:" + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+    }
+}
--- spring-14-Data-Queries/src/main/java/com/cydeo/entity/BaseEntity.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,7 +1,5 @@
 package com.cydeo.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -9,7 +7,5 @@ import javax.persistence.MappedSuperclass;
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Integer id;
 }
