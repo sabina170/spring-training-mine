fdc3dab7402b4656ef51418f251f34d4ef316885
Sabina Omarova <sabomar170@gmail.com>
2022-10-31 11:25:16 -0400

    Spring  - Data Cinema Lab - updated

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/MovieCinema.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/MovieCinema.java
@@ -3,7 +3,9 @@
 package com.cydeo.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,7 +14,8 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class MovieCinema extends BaseEntity {
 
@@ -24,6 +27,10 @@ public class MovieCinema extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Cinema cinema;
 
-
-
+    @Override
+    public String toString() {
+        return "MovieCinema{" +
+                "dateTime=" + dateTime +
+                '}';
+    }
 }
