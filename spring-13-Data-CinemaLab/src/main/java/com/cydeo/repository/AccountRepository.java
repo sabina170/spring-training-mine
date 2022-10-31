dc363ddae97c42dc27fe31d2f21a2d8285364b38
Sabina Omarova <sabomar170@gmail.com>
2022-10-31 11:40:01 -0400

    Spring  - Data Cinema Lab - practice DERIVED QUERIES

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,30 +1,39 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Account;
+import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
-    //Write a derived query to list all accounts with a specific country or state
+    //Write a derived query to list all accounts with a specific country or state:
+    List<Account> findAllByCountryOrState(String country, String state);
 
 
-    //Write a derived query to list all accounts with age lower than or equal to a specific value
+    //Write a derived query to list all accounts with age lower than or equal to a specific value:
+    List<Account> findAllByAgeLessThanEqual(Integer age);
 
 
-    //Write a derived query to list all accounts with a specific role
+    //Write a derived query to list all accounts with a specific role:
+    List<Account> findAllByRole(UserRole role);
 
 
-    //Write a derived query to list all accounts between a range of ages
+    //Write a derived query to list all accounts between a range of ages:
+    List<Account> findAllByAgeBetween(Integer age1, Integer age2);
 
 
-    //Write a derived query to list all accounts where the beginning of the address contains the keyword
+    //Write a derived query to list all accounts where the beginning of the address contains the keyword:
+    List<Account> findAllByAddressStartsWith(String str);
 
 
-    //Write a derived query to sort the list of accounts with age
+    //Write a derived query to sort the list of accounts with age:
+    List<Account> findByOrderByAgeDesc(); // not correct! ---> findByAgeByOrderDesc()
 
 
     // ------------------- JPQL QUERIES ------------------- //
