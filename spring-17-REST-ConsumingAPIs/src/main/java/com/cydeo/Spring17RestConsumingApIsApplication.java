1cc6dac691809afad73788770cc3514a61aea26c
Sabina Omarova <sabomar170@gmail.com>
2022-11-28 12:28:15 -0500

    Spring-17 REST-Consuming APIs

--- spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/Spring17RestConsumingApIsApplication.java
+++ spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/Spring17RestConsumingApIsApplication.java
@@ -1,4 +1,4 @@
-package com.cydeo.spring17restconsumingapis;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
