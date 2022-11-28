f612f04e7781f98235c7569cd0c1d087620e3ff5
Sabina Omarova <sabomar170@gmail.com>
2022-11-28 11:51:45 -0500

    Spring  - 15 REST

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -1,14 +1,12 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.CourseDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/courses/api/v3")
@@ -34,4 +32,17 @@ public class CourseController_ResponseWrapper {
         return ResponseEntity
                 .ok(new ResponseWrapper("course:" + courseId + "retrieved",courseService.getCourseById(courseId)));
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") Long courseId){ // or we can put public ResponseEntity<CourseDTO>
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO courseDTO){
+        courseService.updateCourse(courseId,courseDTO);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- spring-15-REST/src/main/java/com/cydeo/dto/ResponseWrapper.java
+++ spring-15-REST/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -1,7 +1,13 @@
 package com.cydeo.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ResponseWrapper {
 
     private  boolean success;
