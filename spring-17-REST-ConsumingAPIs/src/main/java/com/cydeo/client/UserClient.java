8719c430357bec06f17f018548a4d41012cb1299
Sabina Omarova <sabomar170@gmail.com>
2022-12-02 10:47:08 -0500

    Spring-17 -REST-ConsumingAPIs

--- spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/client/UserClient.java
+++ spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/client/UserClient.java
@@ -1,4 +1,18 @@
 package com.cydeo.client;
 
+import com.cydeo.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(url="https://jsonplaceholder.typicode.com/users", name = "USER-CLIENT")
 public interface UserClient {
+
+    //Whenever we call this getUsers method anywhere in the app->
+    // It will hit/execute these endpoints : https://jsonplaceholder.typicode.com/users/users->
+    // Then, json is coming and assigns to my own UserDto.
+    @GetMapping("/users")
+    List<User> getUsers();
+
 }
