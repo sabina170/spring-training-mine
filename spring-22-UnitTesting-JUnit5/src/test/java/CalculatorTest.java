9fa41f8704993e3416f2b181831152d58c7d2541
Sabina Omarova <sabomar170@gmail.com>
2022-12-15 13:06:45 -0500

    spring-22-UnitTesting -JUnit5

---
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        System.out.println("Add method");
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual, "Test failed.");
+    }
+
+    @Test
+    void testCase1() {
+//        System.out.println("Test Case 1");
+        fail("Not implemented yet.");
+    }
+
+    @Test
+    void testCase2() {
+        System.out.println("Test case 2");
+        assertEquals("add", Calculator.operator);
+        assertTrue(Calculator.operator.equals("add"));
+    }
+
+    @Test
+    void testCase3() {
+        System.out.println("Test case 3");
+        assertArrayEquals(new int []{1,2,3}, new int[]{1,2,3}, "Arrays are not same");
+    }
+
+    @Test
+    void testCase4() {
+        System.out.println("Test case 4");
+        String nullString = null;
+        String notNullString = "Cydeo";
+
+        assertNull(nullString);
+        assertNotNull(notNullString);
+
+       // assertNull(notNullString);
+        assertNotNull(nullString);
+    }
+
+    @Test
+    void testCase5() {
+        System.out.println("Test case 5");
+    }
+
+}
